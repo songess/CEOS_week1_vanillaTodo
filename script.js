a9170972eeebe9143ffd6ce204e36376fb6eab4a
eunsu <songess@naver.com>
2024-03-12 13:36:34 +0900

    feat: todo 추가기능 구현

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//CEOS 19기 프론트엔드 파이팅🔥 ദ്ദി˶ˊᵕˋ˵)
+// todo 추가하기
+const addTodo = document.querySelector('.addTodo');
+const todoInput = document.querySelector('.todoInput');
+
+addTodo.addEventListener('click', () => {
+  if (todoInput.value) {
+    const todoCard = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const todoContent = document.createElement('label');
+    const todoDelete = document.createElement('div');
+
+    todoCard.className = 'todoCard';
+    checkbox.type = 'checkbox';
+    checkbox.className = 'todoCheckbox';
+    checkbox.id = 'todoCheckbox';
+    todoContent.className = 'todoContent';
+    todoContent.htmlFor = 'todoCheckbox';
+    todoContent.textContent = todoInput.value;
+    todoDelete.className = 'todoDelete';
+    todoDelete.textContent = 'X';
+
+    todoCard.append(checkbox, todoContent, todoDelete);
+    document.querySelector('.todoSection').append(todoCard);
+  } else {
+    alert('할 일을 입력해주세요!');
+  }
+});
